6157d852bb8f65096af6b13e52e1d462e4d4e129
NATHANIM-REPO <natimc225@gmail.com>
2024-09-26 13:25:59 +0300

    adding a side bar ddevices that have max-width 800px

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+    function toggleSidebar() {
+        var sidebar = document.getElementById('sidebar2');
+        if (sidebar.style.right === '0px') {
+          sidebar.style.right = '-250px'; 
+        } else {
+          sidebar.style.right = '0px'; 
+        }
+      }
