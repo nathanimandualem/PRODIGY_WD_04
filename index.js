449dbbb8ec675e5eee070ca3f98472e6e0c6da4a
NATHANIM-REPO <natimc225@gmail.com>
2024-09-27 15:13:07 +0300

    fixed the background color when scrolled only for devices that have mx-width 800px

--- index.js
+++ index.js
@@ -8,15 +8,15 @@
           sidebar.style.right = '0px'; 
         }
       }
-      if  (window.matchMedia("(max-width: 800px)").matches) {
+      if (window.matchMedia("(max-width: 800px)").matches) {
       window.addEventListener('scroll', function(){
         const header =  document.getElementById('firstHeader');
-        
+        const icon = document.getElementById('menu-container')
         if(window.scrollY > 50){
           header.style.backgroundColor = '#e1e1e1'
-         
+          icon.style.backgroundColor = '#e1e1e1'
         }
         else{
           header.style.backgroundColor = 'transparent'
-          
+          icon.style.backgroundColor = 'transparent'
         }})}
