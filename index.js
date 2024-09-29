5c07b6128b6b1084df76ad53246e2e207444afc6
NATHANIM-REPO <natimc225@gmail.com>
2024-09-29 18:18:19 +0300

    fixed the header

--- index.js
+++ index.js
@@ -11,12 +11,12 @@
       if (window.matchMedia("(max-width: 800px)").matches) {
       window.addEventListener('scroll', function(){
         const header =  document.getElementById('firstHeader');
-        const icon = document.getElementById('menu-container')
+        const icon = document.getElementById('menu-btn')
         if(window.scrollY > 50){
           header.style.backgroundColor = '#e1e1e1'
           icon.style.backgroundColor = '#e1e1e1'
         }
         else{
           header.style.backgroundColor = 'transparent'
-          icon.style.backgroundColor = 'transparent'
+          icon.style.backgroundColor = 'rgb(246, 240, 240)'
         }})}
